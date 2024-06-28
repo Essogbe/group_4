458dd973b720aaa8f681577ba6f901cb64884abf
elfried96 <134107604+elfried96@users.noreply.github.com>
2024-06-28 22:06:51 +0100

    Update numpy.py
    elfried part add

--- task_1/numpy.py
+++ task_1/numpy.py
@@ -29,3 +29,26 @@ class Array:
         else:
             return self.data[index]  # Tableau 1D
 
+    def __len__(self) -> int:
+        return len(self.data) if self.shape == (len(self.data),) else self.shape[0]
+              
+        # Pour obtenir un élément spécifique ou une tranche du tableau
+    def __getitem__(self, index: Union[int, Tuple[int, int]]) -> Union[int, List[int]]:
+        
+        if isinstance(index, tuple):
+            return self.data[index[0]][index[1]]  # Tableau 2D
+        else:
+            return self.data[index]  # Tableau 1D      
+    # Le produit scalaire avec deux Tableau 1d et de même taille 
+
+    def __matmul__(self , other:'Array')->Union[int , float]:
+        if self.shape != other.shape or len(self.data)!= len(other.data): 
+            raise ValueError ('Cette opération est uniquement sur les array 1d et qui sont de même taille')
+        return sum(element_self * element_other for element_self, element_other in zip(self.data, other.data))
+    # Fonction pour la Recherche avec l'opérateur 'in'
+    def __contains__(self, item: Any) -> bool:
+        if len(self.shape) == 1:
+            return item in self.data
+        else:
+            return any(item in row for row in self.data)
+
