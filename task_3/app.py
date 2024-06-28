dd4d290c7659db00f169765735bebe1c5b3b4968
OGOUBIYI Camus Ismael Kareem <136015435+IsmaelOGOUBIYI@users.noreply.github.com>
2024-06-28 18:59:06 +0100

    Ajout de la fonction save_image et du bouton save

--- task_3/app.py
+++ task_3/app.py
@@ -26,6 +26,10 @@ class App:
         self.generate_button = ctk.CTkButton(self.input_frame, text="Generate", height=40, command=self.generate_image)
         self.generate_button.grid(row=1, column=0, columnspan=2, sticky="news", padx=25, pady=10)
 
+        self.save_button = ctk.CTkButton(self.input_frame, text="Save", height=40, command=self.save_image)
+        self.save_button.grid(row=2, column=0, columnspan=2, sticky="news", padx=25, pady=10)
+        self.save_button.grid_remove()
+
         self.pipe = StableDiffusionPipeline.from_pretrained("hf-internal-testing/tiny-stable-diffusion-torch")
 
         self.progressbar = ctk.CTkProgressBar(self.root, mode="indeterminate")
@@ -43,6 +47,7 @@ class App:
             # Generation de l'image
             image = self.pipe(description).images[0]
             resized_image = image.resize((512, 512), resample=Image.LANCZOS)
+            self.generated_image = resized_image
             image_tk = ImageTk.PhotoImage(resized_image)
             self.root.after(10, self.progressbar.stop)
             self.root.after(10,self.progressbar.pack_forget)  
@@ -51,6 +56,18 @@ class App:
 
         except Exception as e:
             print(f"Error generating image: {e}")
+
+    def save_image(self):
+        if self.generated_image:
+            file_path = ctk.filedialog.asksaveasfilename(defaultextension=".png",
+                                                         filetypes=[("PNG files", "*.png"),
+                                                                ("JPEG files", "*.jpg"),
+                                                                ("All files", "*.*")])
+            if file_path:
+                self.generated_image.save(file_path)
+                print(f"Image saved to {file_path}")
+            else:
+                print("No image to save")
 
 if __name__ == "__main__":
     root = ctk.CTk()
